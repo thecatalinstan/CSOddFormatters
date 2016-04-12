41dbcf4b231969617ac77979028c98b3197d1c63
Cătălin Stan <catalin.stan@me.com>
2016-04-12 12:26:28 +0200

    Setup project boilerplate

---
+++ CSSinceFormatter/CSSinceFormatter/CSSinceFormatter.h
@@ -0,0 +1,19 @@
+//
+//  CSSinceFormatter.h
+//  CSSinceFormatter
+//
+//  Created by Cătălin Stan on 12/04/16.
+//  Copyright © 2016 Cătălin Stan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CSSinceFormatter.
+FOUNDATION_EXPORT double CSSinceFormatterVersionNumber;
+
+//! Project version string for CSSinceFormatter.
+FOUNDATION_EXPORT const unsigned char CSSinceFormatterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSSinceFormatter/PublicHeader.h>
+
+
---
+++ CSSinceFormatter/CSSinceFormatterTests/CSSinceFormattertests.m
@@ -0,0 +1,39 @@
+//
+//  CSSinceFormattertests.m
+//  CSSinceFormatter
+//
+//  Created by Cătălin Stan on 12/04/16.
+//  Copyright © 2016 Cătălin Stan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CSSinceFormattertests : XCTestCase
+
+@end
+
+@implementation CSSinceFormattertests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
